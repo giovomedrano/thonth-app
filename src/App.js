a1d43308a56644c4b8191d636099d293818aa7c7
Giovanni Medrano <giomedrano.ing@gmail.com>
2022-07-14 20:06:46 -0400

    App tittle updated

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App({ signOut }) {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>THONTH App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
         placeholder="Note name"
