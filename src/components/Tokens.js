dc9013a2cb4f19254226f98caa94661e40e86cba
Giovanni Medrano <giomedrano.ing@gmail.com>
2022-07-19 18:26:11 -0400

    Fetch data updated

--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { MDBDataTableV5 } from 'mdbreact';
 
-function Tokens( {columns, tokens}) {
+const Tokens = ({columns, tokens}) => {
   const [datatable] = React.useState({
     columns: columns,
     rows: tokens,
-  });
+  })
 
-  return (<MDBDataTableV5
-            scrollX
-            hover
-            entriesOptions={[5, 10, 25]}
-            entries={5}
-            pagesAmount={4}
-            data={datatable}
-            pagingTop
-            searchTop
-            fullPagination
-            searchBottom={false}
-          />
-  );
+  return (
+    <MDBDataTableV5 
+      scrollX
+      hover
+      entriesOptions={[5, 10, 25]}
+      entries={5}
+      pagesAmount={4}
+      data={datatable}
+      pagingTop
+      searchTop
+      fullPagination
+      searchBottom={false}
+    />
+  )
 }
 
 export default Tokens
